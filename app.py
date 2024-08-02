949e2b7d43a4618bdb9b7a856350aad0b4ae51a7
vnarvaezt <vnarvaezt@gmail.com>
2024-08-02 17:13:53 +0200

    created post route for data

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from model import Model
+
+from dotenv import load_dotenv
+
+load_dotenv()
+model = Model()
 
 app = Flask(__name__)
 @app.route('/', methods=['GET'])
-
 def home():
     return "OK !", 200
+
+
+@app.route('/predict', methods=['POST'])
+
+def predict():
+    body = request.get_json()
+    df = pd.read_json(body)
+    results = [int(x) for x in model.predict(df).flatten()]
+    return jsonify(results), 200
+
 
 
 if __name__ == "__main__":
--- model.py
+++ model.py
@@ -8,17 +8,19 @@ model_version = "latest"
 class Model():
     def __init__(self):
         self.registry_name = os.getenv("MLFLOW_REGISTRY_NAME")
+        self.mlflow_server = os.getenv("MLFLOW_SERVER")
         self.load_model()
 
 
     def load_model(self):
-        client = MlflowClient()
+        client = MlflowClient(self.mlflow_server)
+        mlflow.set_tracking_uri(self.mlflow_server)
         # load model
-        model_version = client.get_latest_versions(self.registry_name, stages=["None"])[0]
+        model_version = client.get_latest_versions(name=self.registry_name, stages=["None"])[0]
         model_uri = f"models:/{self.registry_name}/{model_version.version}"
         self.model = mlflow.sklearn.load_model(model_uri)
         # transformed pipeline
-        path_pipeline = client.download_artifacts(model_version.run_id, "transform_pipeline.pkl")
+        path_pipeline = client.download_artifacts(model_version.run_id, "transform_features.pkl")
         self.transform_pipeline = joblib.load(path_pipeline)
 
     def predict(self, X):
